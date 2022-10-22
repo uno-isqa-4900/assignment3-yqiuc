8f7c140a09e17a71a0e75bbce925b45e09c58270
yqiuc <98722550+yqiuc@users.noreply.github.com>
2022-10-22 16:05:17 +0800

    12

--- src/firebaseDb.js
+++ src/firebaseDb.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCpxQdtai27c8r_eIPSMrNOKXZbvJEmOUs',
-    authDomain: 'my-subscriptions-c8f67.firebaseapp.com',
-    projectId: 'my-subscriptions-c8f67',
-    storageBucket: 'my-subscriptions-c8f67.appspot.com',
-    messagingSenderId: '867713070048',
-    appId: '1:867713070048:web:b3424a1ed99fc02fab7185'
+    apiKey: 'AIzaSyC4cdfGyFQ7SZzOEcbCdiWI4tCThmFoeno',
+    authDomain: 'my-subscriptions-c874d.firebaseapp.com',
+    projectId: 'my-subscriptions-c874d',
+    storageBucket: 'my-subscriptions-c874d.appspot.com',
+    messagingSenderId: '1005672645336',
+    appId: '1:1005672645336:web:df24df6235da632f68cdad'
 }
 
 const db = firebase.initializeApp(firebaseConfig);
