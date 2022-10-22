19eaae8a3d8ac9715351e1c7fea8a42204eb27ed
yqiuc <98722550+yqiuc@users.noreply.github.com>
2022-10-22 13:11:01 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,72 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <header>
+      <nav class="navbar justify-content-between flex-nowrap flex-row">
+        <div class="container">
+          <h1><span>Subscriptions</span>App</h1>
+          <ul class="nav navbar-nav flex-row float-right">
+            <li class="nav-item">
+              <router-link class="nav-link pr-3" to="/create">Add Subscription</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">View Subscriptions</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <router-view class="modify"></router-view>
+    </main>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import
+url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@500&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', san-serif;
+
+  &::selection {
+    background: transparentize(#42B883, 0.5);
+  }
 }
 
-nav {
-  padding: 30px;
+body {
+  background-color: #35495B;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+a {
+  text-decoration: none;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
+header {
+  display: flex;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2C3D4E;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    color: #FFF;
+    font-size: 28px;
+    
+    span {
+      color: #42B883;
     }
   }
 }
+
+h3 {
+  padding-top: 10px;
+}
+
+.modify {
+  padding: 10px;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 createApp(App).use(router).mount('#app')
